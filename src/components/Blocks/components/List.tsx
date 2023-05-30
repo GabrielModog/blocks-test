f6186bf0cd3f172fb81c019e6af56d93715fb856
GabrielModog <criativo.modog@gmail.com>
2023-05-30 07:57:48 -0300

    refactor: add query to fetch blocks data

--- src/components/Blocks/components/List.tsx
+++ src/components/Blocks/components/List.tsx
@@ -3,9 +3,9 @@ import { BlockItem } from "./Item";
 
 export function BlockList({ list }: BlocksListProps) {
   if (list?.length === 0 || list === undefined)
-    return <h3>No items to fetch</h3>;
+    return <h3>Nothing yet to show...</h3>;
   return (
-    <div className="grid grid-cols-2 md:grid-cols-7 gap-4 md:gap-2">
+    <div className="grid grid-cols-2 md:grid-cols-5 gap-4 md:gap-2">
       {list.map((block) => (
         <BlockItem key={block.id} {...block} />
       ))}
--- src/components/Blocks/index.tsx
+++ src/components/Blocks/index.tsx
@@ -1,14 +1,34 @@
-import { useQuery } from "react-query";
+import { useInfiniteQuery } from "react-query";
+
 import { BlockList } from "./components/List";
 import { useBlocks } from "./useBlocks";
 
 export default function Blocks() {
   const blocksService = useBlocks();
-  const result = useQuery("blocks", async () => {
-    const fetched = await blocksService.getList();
-    return fetched;
-  });
-  const data = result.data?.data;
 
-  return <BlockList list={data} />;
+  async function fetchBlocks(pageParam = 10) {
+    const fetched = await blocksService.getList(pageParam, 10);
+
+    return fetched.data;
+  }
+
+  const { data, fetchNextPage } = useInfiniteQuery(
+    ["blocks"],
+    ({ pageParam = 10 }) => fetchBlocks(pageParam),
+    {
+      getNextPageParam: (lastPage, pages) => {
+        if (pages.length === 4) return false;
+        return pages.length * 10 + 10;
+      },
+    }
+  );
+
+  const pages = data?.pages;
+
+  return (
+    <>
+      {pages &&
+        pages.map((page, index) => <BlockList key={index} list={page} />)}
+    </>
+  );
 }
