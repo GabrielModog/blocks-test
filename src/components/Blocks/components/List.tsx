40c946ec154105899b19ff4cda386e5269bd3787
GabrielModog <criativo.modog@gmail.com>
2023-05-30 08:40:30 -0300

    feat: add infinity scroll

--- src/components/Blocks/components/List.tsx
+++ src/components/Blocks/components/List.tsx
@@ -1,14 +1,21 @@
+import { forwardRef } from "react";
+
 import { BlocksListProps } from "../Blocks.types";
 import { BlockItem } from "./Item";
 
-export function BlockList({ list }: BlocksListProps) {
-  if (list?.length === 0 || list === undefined)
-    return <h3>Nothing yet to show...</h3>;
-  return (
-    <div className="grid grid-cols-2 md:grid-cols-5 gap-4 md:gap-2">
-      {list.map((block) => (
-        <BlockItem key={block.id} {...block} />
-      ))}
-    </div>
-  );
-}
+export const BlockList = forwardRef<HTMLDivElement, BlocksListProps>(
+  function BlockList({ list }, ref) {
+    if (list?.length === 0 || list === undefined)
+      return <h3>Nothing yet to show...</h3>;
+    return (
+      <div
+        ref={ref}
+        className="grid grid-cols-2 md:grid-cols-5 gap-4 md:gap-2 my-2"
+      >
+        {list.map((block) => (
+          <BlockItem key={block.id} {...block} />
+        ))}
+      </div>
+    );
+  }
+);
--- src/components/Blocks/index.tsx
+++ src/components/Blocks/index.tsx
@@ -1,13 +1,15 @@
+import { useEffect, useRef } from "react";
 import { useInfiniteQuery } from "react-query";
 
 import { BlockList } from "./components/List";
 import { useBlocks } from "./useBlocks";
 
 export default function Blocks() {
+  const observerTarget = useRef(null);
   const blocksService = useBlocks();
 
   async function fetchBlocks(pageParam = 10) {
-    const fetched = await blocksService.getList(pageParam, 10);
+    const fetched = await blocksService.getList(pageParam, 20);
 
     return fetched.data;
   }
@@ -17,7 +19,6 @@ export default function Blocks() {
     ({ pageParam = 10 }) => fetchBlocks(pageParam),
     {
       getNextPageParam: (lastPage, pages) => {
-        if (pages.length === 4) return false;
         return pages.length * 10 + 10;
       },
     }
@@ -25,10 +26,32 @@ export default function Blocks() {
 
   const pages = data?.pages;
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        if (entries[0].isIntersecting) {
+          fetchNextPage();
+        }
+      },
+      { threshold: 1 }
+    );
+
+    if (observerTarget.current) {
+      observer.observe(observerTarget.current);
+    }
+
+    return () => {
+      if (observerTarget.current) {
+        observer.unobserve(observerTarget.current);
+      }
+    };
+  }, [observerTarget]);
+
   return (
     <>
       {pages &&
         pages.map((page, index) => <BlockList key={index} list={page} />)}
+      <div ref={observerTarget} />
     </>
   );
 }
